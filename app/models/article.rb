66dffebb2ed32e5fc26d1ec5709769f63e0f4618
Victoria B <viktorija.bourassa@gmail.com>
2019-03-02 16:52:39 +0000

    Automatically backed up by Learn

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,18 +1,4 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
-  # attr_accessor :title, :content
-  # 
-  # @@all = []
-  # 
-  # def initialize()
-  #   @title = title
-  #   @content = content
-  #   @@all << self
-  # end
-  # 
-  # def self.all 
-  #   @@all
-  # end
-  # 
 end
