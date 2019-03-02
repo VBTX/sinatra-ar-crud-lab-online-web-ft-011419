ebacb8664f02e5af7969132bb54994d2254abbc6
Victoria B <viktorija.bourassa@gmail.com>
2019-03-02 16:58:09 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,7 +43,7 @@ end
   patch "/articles/:id" do
   @article = Article.find(params[:id])
   @article.update(params[:article])
-  redirect to "/articles/#{ @article.id }"
+  redirect to "/articles/#{@article.id}"
   end
 
   delete '/articles/:id' do
